34e56ebcfa5b03ad24d447d08a7f852c09b44840
Dmytro Kruhlov <d.kruglov1917@mail.ru>
2022-01-12 20:38:11 +0100

    water

---
+++ Assets/_Scripts/Environment/Water/WaterManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+
+public class WaterManager : MonoBehaviour
+{
+    private MeshFilter meshFilter;
+
+    private void Awake()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+    }
+
+    private void Update()
+    {
+        Vector3[] verticies = meshFilter.mesh.vertices;
+        for (int i = 0; i < verticies.Length; i++)
+        {
+            verticies[i].y = WaveManager.instance.GetWaveHeight(transform.position.x + verticies[i].x);
+        }
+
+        meshFilter.mesh.vertices = verticies;
+        meshFilter.mesh.RecalculateNormals();
+    }
+}
